d6c7351cb441369be864eb0e6b31f2cfa814bb51
Dmitry <red5288@gmail.com>
2022-09-18 01:25:06 +0300

    new

---
+++ game_worm/js/script.js
@@ -0,0 +1,116 @@
+const canvas = document.getElementById('worm');
+const ctx = canvas.getContext('2d');
+
+const pole = new Image();
+pole.src = 'img/1.png';
+
+const foodImg = new Image();
+foodImg.src = 'img/food.png';
+
+const snakeImg = new Image();
+snakeImg.src = 'img/snak.png';
+
+const finishSnakeImg = new Image();
+finishSnakeImg.src = 'img/food.png';
+
+const popap = document.getElementById('newElem');
+
+const box = 50;
+let caunt = 0;
+
+let dir;
+
+
+let food = {
+    x: Math.floor(Math.random() * 12 + 1) * box,
+    y: Math.floor(Math.random() * 10 + 1) * box
+};
+
+let snake = [];
+snake[0] = {
+    x: 6 * box,
+    y: 5 * box
+};
+
+document.addEventListener("keydown", (e));
+
+function e (event) {
+    if(event.keyCode == 37 && dir !='right')
+    dir = 'left';
+    else if(event.keyCode == 38 && dir !='down')
+    dir = 'up';
+    else if(event.keyCode == 39 && dir !='left')
+    dir = 'right';
+    else if(event.keyCode == 40 && dir !='up')
+    dir = 'down';
+}
+
+function eatSnakeSelf (head, body) {
+    for (i=0; i<body.length; i++) {
+        if (head.x == body[i].x && head.y == body[i].y) {
+        popap.style.display = 'block';
+    }
+    }
+}
+
+function drawGame () {
+    ctx.drawImage(pole, 0, 0);
+    ctx.drawImage(foodImg, food.x, food.y, box, box);
+
+    for(let i=0; i<snake.length; i++) {
+        ctx.drawImage( i == 0? snakeImg: finishSnakeImg, snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = 'white';
+    ctx.font = '50px Arial';
+    ctx.fillText(caunt, box*5.2, box*0.9);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (snakeX == food.x && snakeY == food.y) {
+        caunt++;
+        food = {
+            x: Math.floor(Math.random() * 12 + 1) * box,
+            y: Math.floor(Math.random() * 10 + 1) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+    if (snakeX < box || snakeX > box * 12 || snakeY < box|| snakeY > box * 10) {
+        popap.style.display = 'block';
+    }
+
+
+    if (dir=='left') snakeX -= box;
+    if (dir=='right') snakeX += box;
+    if (dir=='up') snakeY -= box;
+    if (dir=='down') snakeY += box;
+
+    let run = {
+        x:snakeX,
+        y:snakeY,
+    };
+
+    eatSnakeSelf (run, snake)
+
+    snake.unshift(run);
+
+}
+
+let game = setInterval(drawGame, 200);
+
+
+let newRepits = () => {
+    snake = [];
+    snake[0] = {
+        x: 6 * box,
+        y: 5 * box
+    };
+    dir = 0;
+    caunt = 0;
+    if (popap.style.display = 'block')
+    popap.style.display = 'none';
+    
+}
